3025731834972c60e1576172596f0c22c59c30fc
mehediH-shakil <mehedi.vu.cse@gmail.com>
2026-07-06 23:46:28 +0600

    Remove unused slideshow CSS and JS files, and update Education to list view

--- static/js/education.js
+++ static/js/education.js
@@ -5,30 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
             let workbook = XLSX.read(data, { type: "array" });
             let education = getSheetData(workbook, 'Education');
             const listContainer = document.getElementById("education-list-container");
-            const dotsContainer = document.getElementById("education-dots-container");
-            if (!listContainer || !dotsContainer) return;
+            if (!listContainer) return;
 
             let html = "";
-            let dotsHtml = "";
             education.forEach(edu => {
                 html += `
-                <div class="slide">
-                  <div class="slide-content">
-                    <h4>${edu.institution}</h4>
-                    <p><b>${edu.degree}</b></p>
-                    <p>${edu.duration}</p>
-                    <p>${edu.details}</p>
-                  </div>
+                <div class="row clearfix layout layout-left">
+                    <div class="col-xs-12 col-print-12 details">
+                        <h4>${edu.institution}</h4>
+                        <p><b>${edu.degree}</b></p>
+                        <p>${edu.duration}</p>
+                        <p>${edu.details}</p>
+                    </div>
                 </div>`;
-                dotsHtml += `<span class="dot"></span>\n`;
             });
             listContainer.innerHTML = html;
-            dotsContainer.innerHTML = dotsHtml;
-            
-            if (typeof showSlides === 'function') {
-                slideIndex = 1;
-                showSlides(slideIndex);
-            }
         })
         .catch(err => console.error(err));
 });
